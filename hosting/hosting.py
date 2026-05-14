9bbafef3a78a5d70c90d7a52f45474878488ca7f
vinaykumartv-glut <vinaykumar.tv@gmail.com>
2026-05-14 16:48:43 +0000

    Add MLOps pipeline files

--- hosting/hosting.py
+++ hosting/hosting.py
@@ -3,7 +3,7 @@ import os
 
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
-    folder_path="tourism-project/deployment",     # the local folder containing your files
+    folder_path="deployment",     # the local folder containing your files
     repo_id="vinaykumartv/tourism-project-data",          # the target repo
     repo_type="space",                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
